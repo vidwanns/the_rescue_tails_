1cfa11916acaa77672b89f255f0a2592cb3167c8
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-10-15 21:27:23 +0530

    first commit

---
+++ src/app/components/layout/header.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import '../../styles/component/layout/header.css';
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <div className="logo">
+        <img src="/images/header/logo.svg" alt="The Rescue Tails Logo" />
+      </div>
+
+      <nav className="nav-links">
+        <Link href="#home" passHref>
+          <div>Home</div>
+        </Link>
+        <Link href="#about" passHref>
+          <div>About</div>
+        </Link>
+        <Link href="#community" passHref>
+          <div>Community</div>
+        </Link>
+        <Link href="#volunteer" passHref>
+          <div>Volunteer</div>
+        </Link>
+        <Link href="#adopt" passHref>
+          <div>Adopt</div>
+        </Link>
+      </nav>
+
+      <div className="buttons-container">
+        <div className="button contact">contact us</div>
+        <div className="button donate">Donate</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
