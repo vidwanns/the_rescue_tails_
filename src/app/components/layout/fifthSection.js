01989063021db04840ffdbed827c0df52a6f2de1
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-10-23 07:29:36 +0530

    changes

--- src/app/components/layout/fifthSection.js
+++ src/app/components/layout/fifthSection.js
@@ -3,7 +3,8 @@ import '../../styles/component/fifthSection/fifthSection.css';
 
 const FifthSection = () => {
     return (
-      <section className="fifth-section">
+      <section className="fifth-section container">
+        <div className="fifth_content">
         <div className="top-row">
           <img src="/images/fifthSection/girl-1.png" alt="Woman with pets" className="top-image" />
           <div className="instagram-card">
@@ -17,6 +18,7 @@ const FifthSection = () => {
           <img src="/images/fifthSection/girl-1.png" alt="Cat being pet" className="bottom-image" />
           <img src="/images/fifthSection/girl-1.png" alt="Dog paw in hand" className="bottom-image" />
           <img src="/images/fifthSection/girl-1.png" alt="Dog in jacket" className="bottom-image" />
+        </div>
         </div>
       </section>
     );
