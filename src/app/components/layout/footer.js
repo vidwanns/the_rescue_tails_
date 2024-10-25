abbb21b205201a8c399e724208d9e9aeb10dd6ba
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-10-26 03:23:26 +0530

    footer

--- src/app/components/layout/footer.js
+++ src/app/components/layout/footer.js
@@ -1,38 +1,49 @@
-import React from 'react';
-import '../../styles/component/layout/footer.css';
+import React from "react";
+import "../../styles/component/layout/footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="outer-container-footer">
         <div className="inner-container-footer">
-          <div className="footer-logo">
-            {/* Add your SVG logo here */}
-            <svg className="logo-svg"></svg>
+          {/* Logo at the Start */}
+          <div className="logo">
+            <img src="/images/footer/logo.svg" alt="The Rescue Tails Logo" />
           </div>
-          <div className="footer-links">
-            <ul className="links-column">
-              <li>Home</li>
-              <li>Adopt</li>
-              <li>Home</li>
-            </ul>
-            <ul className="links-column">
-              <li>About Us</li>
-              <li>Contacts</li>
-              <li>How to Help</li>
-            </ul>
-          </div>
-          <div className="footer-contact">
-            <p>Awwars and manchester, blu</p>
-            <p>Pincode-901503</p>
-            <p>+999999999999</p>
-            <p>hello@gmail.com</p>
-          </div>
-          <div className="footer-donate">
-            <button className="donate-btn">
-              Donate
-              <span className="arrow">→</span>
-            </button>
+
+          {/* Text Content Positioned to the Right */}
+          <div className="text-container">
+            <div className="text-wrapper">
+              {/* Footer Links (Left side of contact) */}
+              <div className="footer-links">
+                <ul className="links-column">
+                  <li>Home</li>
+                  <li>Adopt</li>
+                  <li>Home</li>
+                </ul>
+                <ul className="links-column">
+                  <li>About Us</li>
+                  <li>Contacts</li>
+                  <li>How to Help</li>
+                </ul>
+              </div>
+
+              {/* Footer Contact at Top Right */}
+              <div className="footer-contact">
+                <p>Awwars and manchester, blu</p>
+                <p>Pincode-901503</p>
+                <p>+999999999999</p>
+                <p>hello@gmail.com</p>
+              </div>
+            </div>
+
+            {/* Donate Button below */}
+            <div className="footer-donate">
+              <button className="donate-btn">
+                Donate
+                <span className="arrow">→</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
